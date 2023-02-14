25989c1c0c93837efd0741181dbb3369805f7c9c
10matcho27 <ryo.1027.smile@gmail.com>
2023-02-14 16:24:36 +0900

    first commit

---
+++ 2D_Simuration/2DSim.py
@@ -0,0 +1,79 @@
+#png→Gif
+#https://photocombine.net/gifanime/
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import matplotlib.animation as animation
+
+def init_stat(t, alfa, τ0):
+    num = math.exp(-1.0 * (alfa * (t - τ0)) ** 2.0)
+    return num
+
+def calc_Ez(i, j, Ez_t1, cv, dt, dx, dy, By_t2, Bx_t2):
+    return Ez_t1[i][j] + (cv**2) * dt * ((By_t2[i][j] - By_t2[i][j-1]) / dx - (Bx_t2[i][j] - Bx_t2[i-1][j]) / dy)
+
+def calc_By(i, j, By_t1, Ez_t2, dt, dx):
+    return By_t1[i][j] + (dt/dx) * (Ez_t2[i][j+1] - Ez_t2[i][j])
+
+def calc_Bx(i, j, Bx_t1, Ez_t2, dt, dy):
+    return Bx_t1[i][j] - (dt/dy) * (Ez_t2[i+1][j] - Ez_t2[i][j])
+
+def main():
+    Nx, Ny, cv, dx, dy =300, 300, 5.0, 2.0, 2.0
+    dt = dx / 10.0    #dt < dx
+    τ0 = dt * 400.0 #τ0 >> dt
+    alfa = (2.0/τ0)
+    total_step = int((dx*Nx)/(cv*dt)) * 2
+
+    #setup for Ez, By, Bx
+    Ez_t1, Ez_t2, By_t1, By_t2, Bx_t1, Bx_t2 = \
+        [[0 for _ in range(Nx)] for __ in range(Nx)], [[0 for _ in range(Nx)] for __ in range(Nx)],\
+            [[0 for _ in range(Nx)] for __ in range(Nx)], [[0 for _ in range(Nx)] for __ in range(Nx)],\
+                [[0 for _ in range(Nx)] for __ in range(Nx)], [[0 for _ in range(Nx)] for __ in range(Nx)]
+    Ez_t1, Ez_t2, By_t1, By_t2, Bx_t1, Bx_t2 = \
+        np.array(Ez_t1), np.array(Ez_t2), np.array(By_t1), np.array(By_t2), np.array(Bx_t1), np.array(Bx_t2)
+    Ez_t1, Ez_t2, By_t1, By_t2, Bx_t2, Bx_t2 = \
+        np.float_(Ez_t1), np.float_(Ez_t2), np.float_(By_t1), np.float_(By_t2), np.float_(Bx_t1), np.float_(Bx_t2)
+
+    fig = plt.figure()
+    image_list = []
+
+    for step_num in tqdm(range(total_step)): #loop of time x,y方向
+        Ez_t1[1][1] = init_stat(step_num, alfa, τ0) #格子番号1のEzに初期値(時間関数)を与える
+        for i in range(1, Ny-1):
+            for j in range(1, Nx-1):
+                Ez_t2[i][j] = calc_Ez(i, j, Ez_t1, cv, dt, dx, dy, By_t2, Bx_t2)
+
+        # Ez_t2[0][0], Ez_t2[0][Nx-1], Ez_t2[Ny-1][0], Ez_t2[Nx-1][Ny-1] = 0, 0, 0, 0
+
+        for j in range(Nx):
+            Ez_t2[0][i] = 0
+            Ez_t2[Ny-1][i] = 0
+        for i in range(Ny):
+            Ez_t2[i][0] = 0
+            Ez_t2[i][Nx-1] = 0
+
+        for i in range(1, Ny-1):
+            for j in range(1, Nx-1):
+                By_t2[i][j] = calc_By(i, j, By_t1, Ez_t2, dt, dx)
+
+        for j in range(1, Nx-1):
+            for i in range(1, Ny-1):
+                Bx_t2[i][j] = calc_Bx(i, j, Bx_t1, Ez_t2, dt, dy)
+
+        im = plt.imshow(Ez_t1, animated=True)
+        image_list.append([im])
+
+        Ez_t1 = Ez_t2
+        By_t1 = By_t2
+        Bx_t1 = Bx_t2
+        plt.savefig("./fig2/fig" + str(step_num) + ".png")
+
+    ani = animation.ArtistAnimation(fig, image_list, interval=1)
+    ani.save("test2.gif", writer="pillow")
+    # plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ particle_orbit/particle_orbit.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
+
+class Particle():
+    def __init__(self, pos = [0.0, 0.0, 0.0], v = [0.0, 0.0, 0.0], mass = 1.0, q = 1):
+        self.pos = np.array(pos)
+        self.v = np.array(v)
+        self.mass = mass
+        self.q = q #正の電荷
+        self.x_all, self.y_all, self.z_all, self.energy_all = [], [], [], []
+
+    def set(self, pos, v, mass, q):
+        self.pos = pos
+        self.v = v
+        self.mass = mass
+        self.q = q
+
+    def append_pos(self, x, y, z):
+        self.x_all.append(x)
+        self.y_all.append(y)
+        self.z_all.append(z)
+
+    def force(self, v, E, B):
+        # return self.q / self.mass *(E + np.cross(v, B))
+        return \
+            np.array([self.q / self.mass * (E[0] + v[1]*B[2] - v[2]*B[1]), \
+                    self.q / self.mass * (E[1] + v[2]*B[0] - v[0]*B[2]), \
+                    self.q / self.mass * (E[2] + v[0]*B[1] - v[1]*B[0])])
+
+    def simply_update(self, dt, E, B):
+        self.v = self.v + dt * self.force(self.v, E, B)
+        self.pos = dt * self.v
+        self.energy()
+
+    def runge_kutta_update(self, dt, E, B):
+        k1 = dt * self.force(self.v, E, B)
+        k2 = dt * self.force((self.v + k1/2), E, B)
+        k3 = dt * self.force((self.v + k2/2), E, B)
+        k4 = dt * self.force((self.v + k3), E, B)
+
+        k = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+        self.v = self.v + k
+        self.pos = dt * self.v
+        self.energy()
+
+    def energy(self):
+        self.energy_all.append((1/2) * self.mass * (self.v)**2)
+
+if __name__ == '__main__':
+    E = np.array([0, 0, 1])
+    B = np.array([0, 0, 1])
+    time_step = 1000
+    dt = 1 / (50)
+    t = np.arange(0, time_step * dt, dt)
+    init_pos = np.array([0, 0, 0])
+
+    #generate particle
+    p0, p1 = Particle(), Particle()
+    p0.set(init_pos, np.array([1, 0, 0]), 1, 1)
+    p1.set(init_pos, np.array([1, 0, 0]), 1, 1)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    # ax = fig.add_subplot()
+    # plt.title("Particle orbit")
+    fig.suptitle("Particle orbit\n[Ex, Ey, Ez]=["+str(E[0])+","+str(E[1])+","+str(E[2])+"]\n[Bx, By, Bz]=["+str(B[0])+","+str(B[1])+","+str(B[2])+"]")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    ax.grid()
+    ax.view_init(elev=5, azim=30)
+    ax.set_aspect('auto')
+    image_list = []
+
+    for t_ in tqdm(range(0, time_step)):
+        # if(t_ == 0):
+        #     print("\n", "init position :", "\n", p0.pos ,"\n", "init v :", "\n", p0.v)
+        p0.simply_update(dt, E, B)
+        p1.runge_kutta_update(dt, E, B)
+
+        p0.append_pos(p0.pos[0], p0.pos[1], p0.pos[2])
+        p1.append_pos(p1.pos[0], p1.pos[1], p1.pos[2])
+
+        im1 = plt.plot(p0.pos[0], p0.pos[1], p0.pos[2], 'o', color = 'red', label="Euler")
+        im1_line = plt.plot(p0.x_all, p0.y_all, p0.z_all, '--', color="red")
+        im2 = plt.plot(p1.pos[0], p1.pos[1], p1.pos[2], '*', color = "blue", label="Runge-Kutta")
+        im2_line = plt.plot(p1.x_all, p1.y_all, p1.z_all, '', color = "blue")
+
+        if(t_ == 0):
+            ax.legend(loc=2)
+            ax.set_xlim([-0.03, 0.03])
+            ax.set_ylim([-0.03, 0.03])
+            ax.set_zlim([0, 1])
+
+        image_list.append(im1 + im2 + im1_line + im2_line)
+
+    ani = animation.ArtistAnimation(fig, image_list, interval=5)
+    plt.show()
+    # ani.save("orbit_comp_3D.gif", writer='pillow')
+    # ani.save("orbit_comp_3D_6.mp4", writer='ffmpeg')
